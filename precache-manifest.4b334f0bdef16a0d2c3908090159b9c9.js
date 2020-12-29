e32ba06f8fa9ac1dbe15125cc819844b16659a74
Bahyu Sanciko <cbahyu@gmail.com>
2020-12-29 08:17:22 +0700

    Updates

---
+++ precache-manifest.4b334f0bdef16a0d2c3908090159b9c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf3a12ae7b883918aaed395eefcf2d3",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "7b0b9b8a3f7d09b3d283",
+    "url": "/blog/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "02c361f2fa9257da9a02",
+    "url": "/blog/static/js/2.af75f974.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/blog/static/js/2.af75f974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b0b9b8a3f7d09b3d283",
+    "url": "/blog/static/js/main.b348bfb6.chunk.js"
+  },
+  {
+    "revision": "75ed156f7d81793dd2ff",
+    "url": "/blog/static/js/runtime-main.b1c61ad7.js"
+  }
+]);
