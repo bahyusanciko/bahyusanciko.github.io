0ca33216ab6ad1864b12087a3c83d1683b066701
Bahyu Sanciko <cbahyu@gmail.com>
2022-06-04 08:41:47 +0700

    build

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 ```javascript
 const aboutme = {
   code: [PHP, Javascript, Typescript, HTML, CSS],
-  tools: [ Laravel, Codeigniter , Angular , Ionic, Styled-Components, Docker],
+  tools: [ Laravel, Codeigniter , Angular , Ionic, React, Bootstrap, Docker],
   architecture: ["microservices", "event-driven", "design system pattern"],
   
  challenge: "I am doing the #100DaysOfCode challenge focused on typescript"
---
+++ precache-manifest.60ce7247f83eb8cbc665d50890d44435.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804fa5fd88a463b8760fab7a0856e6a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "495dda9767258cde0f16",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "02c361f2fa9257da9a02",
+    "url": "/static/js/2.af75f974.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.af75f974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "495dda9767258cde0f16",
+    "url": "/static/js/main.2937a9a5.chunk.js"
+  },
+  {
+    "revision": "ae064dcd756ea8b1993d",
+    "url": "/static/js/runtime-main.ca9bb6fa.js"
+  }
+]);
